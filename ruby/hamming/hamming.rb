1459893593f0a6ad3d86139f574e3adb81df1b27
Brandon Ferguson <brfergus2@gmail.com>
2017-05-09 14:02:31 -0400

    hamming revision 3

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,7 @@
 class Hamming
   def self.compute(a,b)
-    if a == b
-      return 0
-    end
-    strand1 = a.chars
-    strand2 = b.chars
-    raise ArgumentError, 'Strand length not the same' unless strand1.length == strand2.length
-    strand1 = strand1.zip(strand2)
-    distance = strand1.count{|x| x[0]!=x[1]}
+    raise ArgumentError, 'Strand length not the same' unless a.length == b.length
+    a.chars.zip(b.chars).count{|x| x[0]!=x[1]}
   end
 end
 
