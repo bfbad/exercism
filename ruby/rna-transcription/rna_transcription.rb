88358e3ce03dde0b7422bf10a3f343c63ed1a080
Brandon Ferguson <brfergus2@gmail.com>
2017-05-17 14:40:34 -0400

    rna transcription fix spacing

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,11 +1,10 @@
 class Complement 
  
   def self.of_dna(dna)
-  	unless dna =~ /\A[GCTA]+\z/
-  	  return ''
-  	end
-    complement_nts = { 'G' => 'C', 'C' => 'G', 
-    	              'T' => 'A', 'A' => 'U' }
+    unless dna =~ /\A[GCTA]+\z/
+      return ''
+    end
+    complement_nts = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
     rna = [];
     dna.chars.each{ |x| rna.push(complement_nts[x]) }
     rna.join
